23894fd6940e715fa3211e9303ea8937c92de83d
eminaliyev94 <emin.eliyev.94@gmail.com>
2017-07-06 11:12:27 +0400

    hhh

--- ICS/Models/ChangePassword.cs
+++ ICS/Models/ChangePassword.cs
@@ -7,6 +7,7 @@ namespace ICS.Models
         [Key]
         public int id { get; set; }
         [Required(ErrorMessage = "***")]
+        [StringLength(50, ErrorMessage = "Şifrə 5-50 simvol aralığında ola bilər", MinimumLength = 5)]
         public string NewPassword { get; set; }
         [Required(ErrorMessage = "***")]
         [Compare("NewPassword", ErrorMessage = "Şifrələr eyni deyil")]
